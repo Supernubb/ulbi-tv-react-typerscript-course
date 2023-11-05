007d9a979ff1a3fd836e2843291f70affe368058
Vladimir Kosarev <painkiller_1999@mail.ru>
2023-11-05 23:17:07 +0300

    курс Тимура по typescript + react 20:49

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from 'react'
+import Card, { CardVariant } from './components/Card'
+import UserList from './components/UserList'
+import { IUser } from './types/types'
+import axios from 'axios'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+    const [users, setUsers] = useState<IUser[]>([]);
+
+    useEffect(() => {
+        fetchUsers();
+    }, [])
+
+    const fetchUsers = async () => {
+        try {
+            const response = await axios.get<IUser[]>(`https://jsonplaceholder.typicode.com/users`);
+            setUsers(response.data);
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    return (
+        <div>
+            <Card onClick={(num) => console.log(`click`, num)} variant={CardVariant.outlined} width='200px' height='200px'>
+                <button>Button</button>
+                <div>dasdsa</div>
+            </Card>
+            <UserList users={users} />
+        </div>
+    )
 }
 
-export default App;
+export default App
---
+++ src/types/types.tsx
@@ -0,0 +1,12 @@
+export interface IAddres {
+    street: string
+    city: string
+    zipcode: string
+}
+
+export interface IUser {
+    id: number
+    name: string
+    email: string
+    address: IAddres
+}
